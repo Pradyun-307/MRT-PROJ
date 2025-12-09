0bc708c4c15fb0820433ac3f7fdf05c348960b51
Pradyun <pradyunshetty1645@gmail.com>
2025-12-09 20:09:17 +0530

    Update

--- src/path_planning/path_planning/Updatepositon.py
+++ src/path_planning/path_planning/Updatepositon.py
@@ -1,43 +1,64 @@
 import rclpy
 from rclpy.node import Node
 from messages.msg import Coord 
-from messages.msg import rover_path
-from messages.msg import rover_coord
+from messages.msg import RoverPath
+from messages.msg import Rovercoord
 from messages.srv import rover_list
+from messages.msg import RoverAvail
+from messages.srv import RoverAvailList
 class UpdateNode(Node):
     def __init__(self):
         super().__init__('Update_node')
-        self.rover_paths = []
+        self.rover_paths = {}
         self.pointers = {}
         self.subscription = self.create_subscription(
-            rover_path,
+            RoverPath,
             'path',
             self.listener_callback,
             10
         )
-        self.srv = self.create_service(
+        self.srv_update = self.create_service(
             rover_list,
             'Update_position',
             self.update_position_callback
         )
+        self.srv_avail = self.create_service(
+            RoverAvailList,
+            'Available_Rovers',
+            self.Available_position_callback
+        )
 
     def listener_callback(self, msg):
-        self.rover_paths.append(msg)
+        self.rover_paths[msg.roverID] = msg
         self.pointers[msg.roverID] = 0
     def update_position_callback(self, request, response):
         for i in range(len(request.list)):
-            if request.list[i].roverID not in self.pointers:
-                response.list.append(request.list[i])
+            if request.request[i].roverID not in self.pointers:
+                response.response.append(request.request[i])
             else:
-                rid = request.list[i].roverID
-                if self.rover_paths[i].path[self.pointers[rid]] not in response.list:
-                    up = rover_coord()
-                    up.roverID = request.list[i].roverID
-                    up.coord = self.rover_paths[i].path[self.pointers[rid]]
-                    response.list.append(up) 
+                rid = request.request[i].roverID
+                if self.rover_paths[rid].path[self.pointers[rid]] not in response.response:
+                    up = Rovercoord()
+                    up.roverID = request.request[i].roverID
+                    up.coord = self.rover_paths[rid].path[self.pointers[rid]]
+                    response.response.append(up) 
                     self.pointers[rid] += 1
                 else :
-                    response.list.append(request.list[i])
+                    response.response.append(request.request[i])
+                if self.pointers[rid] >= len(self.rover_paths[rid].path):
+                    del self.pointers[rid]
+                    del self.rover_paths[rid]
+        return response
+    def Available_position_callback(self, request, response):
+        for i in range(len(request.request)):
+            up = RoverAvail()
+            rid = request.request[i].roverid
+            avail = request.request[i].avail
+            if rid in self.pointers:
+                avail = False
+            up.roverid = rid
+            up.avail = avail
+            response.response.append(up)
         return response
 def main(args=None):
     rclpy.init(args=args)
